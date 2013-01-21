1a5c6925f79f0433a3574db5062b8e95eeeaebcd
Akim Demaille <akim@lrde.epita.fr>
2013-01-21 12:28:55 +0100

    style: formatting changes
    * src/emit_functions.c, src/emit_kids.c: Reindent.

--- src/emit_functions.c
+++ src/emit_functions.c
@@ -24,55 +24,55 @@
 /** Emit rule functions. */
 void emit_emitter_func ()
 {
-        GList *l;
-        int i, rulen;
-        GHashTable *cache = g_hash_table_new (g_str_hash, g_str_equal);
+  GList *l;
+  int i, rulen;
+  GHashTable *cache = g_hash_table_new (g_str_hash, g_str_equal);
 
-        for (l =  rule_list, i = 0; l; l = l->next) {
-                Rule *rule = (Rule *)l->data;
+  for (l =  rule_list, i = 0; l; l = l->next) {
+    Rule *rule = (Rule *)l->data;
 
-                if (rule->code) {
-                        if ((rulen = GPOINTER_TO_INT (g_hash_table_lookup (cache, rule->code)))) {
-                                emit_rule_string (rule, "");
-                                output ("#define mono_burg_emit_%d mono_burg_emit_%d\n\n", i, rulen - 1);
-                                i++;
-                                continue;
-                        }
-                        output ("static void ");
+    if (rule->code) {
+      if ((rulen = GPOINTER_TO_INT (g_hash_table_lookup (cache, rule->code)))) {
+        emit_rule_string (rule, "");
+        output ("#define mono_burg_emit_%d mono_burg_emit_%d\n\n", i, rulen - 1);
+        i++;
+        continue;
+      }
+      output ("static void ");
 
-                        emit_rule_string (rule, "");
+      emit_rule_string (rule, "");
 
-                        if (dag_mode)
-                                output ("mono_burg_emit_%d (MBState *state, MBTREE_TYPE %ctree, MBCGEN_TYPE *s)\n", i,
-                                        (cxx_ref_p ? '&' : '*'));
-                        else
-                                output ("mono_burg_emit_%d (MBTREE_TYPE %ctree, MBCGEN_TYPE *s)\n", i,
-                                        (cxx_ref_p ? '&' : '*'));
+      if (dag_mode)
+        output ("mono_burg_emit_%d (MBState *state, MBTREE_TYPE %ctree, MBCGEN_TYPE *s)\n", i,
+                (cxx_ref_p ? '&' : '*'));
+      else
+        output ("mono_burg_emit_%d (MBTREE_TYPE %ctree, MBCGEN_TYPE *s)\n", i,
+                (cxx_ref_p ? '&' : '*'));
 
-                        output ("{\n");
-                        output ("\t(void) tree; (void) s;");
-                        if (dag_mode)
-                                output (" (void) state;");
-                        output ("\n\t{\n%s\n\t}\n", rule->code);
-                        output ("}\n\n");
-                        g_hash_table_insert (cache, rule->code, GINT_TO_POINTER (i + 1));
-                }
-                i++;
-        }
+      output ("{\n");
+      output ("\t(void) tree; (void) s;");
+      if (dag_mode)
+        output (" (void) state;");
+      output ("\n\t{\n%s\n\t}\n", rule->code);
+      output ("}\n\n");
+      g_hash_table_insert (cache, rule->code, GINT_TO_POINTER (i + 1));
+    }
+    i++;
+  }
 
-        g_hash_table_destroy (cache);
+  g_hash_table_destroy (cache);
 
-        if (!exported_symbols_p)
-                output ("static ");
-        output ("MBEmitFunc const mono_burg_func [] = {\n");
-        output ("\tNULL,\n");
-        for (l =  rule_list, i = 0; l; l = l->next) {
-                Rule *rule = (Rule *)l->data;
-                if (rule->code)
-                        output ("\tmono_burg_emit_%d,\n", i);
-                else
-                        output ("\tNULL,\n");
-                i++;
-        }
-        output ("};\n\n");
+  if (!exported_symbols_p)
+    output ("static ");
+  output ("MBEmitFunc const mono_burg_func [] = {\n");
+  output ("\tNULL,\n");
+  for (l =  rule_list, i = 0; l; l = l->next) {
+    Rule *rule = (Rule *)l->data;
+    if (rule->code)
+      output ("\tmono_burg_emit_%d,\n", i);
+    else
+      output ("\tNULL,\n");
+    i++;
+  }
+  output ("};\n\n");
 }
